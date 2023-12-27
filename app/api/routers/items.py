37c0b6bc1a52ce9156a84e7e15296c99c598e682
talha915 <talhazafar915426@gmail.com>
2023-12-27 02:23:30 +0100

    changes

--- app/api/routers/items.py
+++ app/api/routers/items.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from api import crud
 from api.models import item as models
+from typing import List
 
 router = APIRouter()
 
@@ -8,3 +9,7 @@ router = APIRouter()
 def create_item(item: models.Item):
     """Create a new item."""
     return crud.create_item(item)
+
+@router.get("/", response_model=List[models.Item])
+async def get_all_items(skip: int=0, limit: int=10):
+    return crud.get_items(skip=skip, limit=limit)    
